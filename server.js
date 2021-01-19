6dc88c2644e775dba3a94b5698cc85e775c4cc22
viktor <viktorkarlsson125@gmail.com>
2021-01-19 16:58:17 +0100

    adding --inspect in scripts for debugging. created fungtionality to add a book, placing all functionality in a const called books

--- server.js
+++ server.js
@@ -9,19 +9,35 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-const getBooks = (request, response, next) => {
-  pool.query('SELECT * FROM books', (error, results) => {
-    if (error) {
-      throw error
-      // console.log(error)
-    } else {
-      response.status(200).json({ books: results.rows })
-    }
-  })
-  // response.json({message: 'Looking Good Mate'})
+const Books = {
+  getBooks(request, response, next) {
+    pool.query('SELECT * FROM books', (error, results) => {
+      if (error) {
+        throw error
+        // console.log(error)
+      } else {
+        response.status(200).json({ books: results.rows })
+      }
+    })
+    // response.json({message: 'Looking Good Mate'})
+  },
+  addBook(request, response, next) {
+    const { author, title } = request.body
+    pool.query(
+      'INSERT INTO books (author, title) VALUES ($1, $2)',
+      [author, title],
+      (error) => {
+        if (error) {throw error}
+        response.status(201).json({message: 'You just created a book buddy!'})
+      }
+    )
+  }
 }
 
-app.route('/books').get(getBooks)
+app
+  .route('/books')
+  .get(Books.getBooks)
+  .post(Books.addBook)
 
 app.listen(3001, () => {
   console.log('Server is up and running...')
